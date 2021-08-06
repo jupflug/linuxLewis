1ff8ba646e9a6a9caa07e40356152fc9873959f2
jupflug <justin.pflug@colorado.edu>
2021-08-06 15:35:04 -0600

    Completed the download routine in the migration package

---
+++ box2Linux_migrate.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from boxMigrate import boxMigrate
+
+from pyproj import Proj
+from pandas import DataFrame
+import numpy as np
+
+id = '4stt8wd7hn5c19obxhh2kpnf7419pni6'
+secret = 'UfS1HnLjbgGeQuh4dtIXBJRT2kVdQC4M'
+token = '7AaEHB77XoAN110zpUf8JwrmDfOU8SfL'
+
+target_dir = 'Rocky_Pflug'
+
+subdirs = ['N40_0W106_0_agg_16','WY2020']
+
+out_path = '/scratch/summit/jupf7869/500m/'
+
+keys = ['Ta_Post','PPT_Post','SWE_Post','SCA_Post']
+
+xMin = 410000; xMax = 463500
+yMin = 4414000; yMax = 4495000
+
+
+# In[3]:
+
+
+x = [xMin, xMax, xMax, xMin]
+y = [yMin, yMin, yMax, yMax]
+df = DataFrame(np.c_[x,y],columns = ['Meters East','Meters South'])
+project = Proj("+proj=utm +zone=13N")
+
+lon, lat = project(df['Meters East'].values, df['Meters South'].values,
+                  inverse=True)
+
+
+# In[4]:
+
+
+client, root_directory = boxMigrate.target_directory(id,secret,token,target_dir)
+print(root_directory)
+
+
+# In[9]:
+
+
+yrRange = range(1985,2021)
+for year in yrRange:
+    subdirs = ['N40_0W106_0_agg_16','WY'+str(year)]
+#     print(subdirs)
+    boxMigrate.download_data(root_directory,subdirs,client,out_path,
+                         filter='latlon',lon = lon,lat = lat,keys = keys)
+
--- boxMigrate/boxMigrate.py
+++ boxMigrate/boxMigrate.py
@@ -1,10 +1,80 @@
 from boxsdk import OAuth2, Client
+import numpy as np
+import netCDF4 as nc
+import os
 
 def iterate_download(folder,name):
     directories = folder.get_items()
     for directory in directories:
         if directory.name == name:
             return directory
+        
+def filterNC_latlon(dsetName,keys,lat,lon,laty,lonx):
+    
+    print(dsetName)
+    
+    dset = nc.Dataset(dsetName)
+    dim_len = len(dset.dimensions)
+#     print(dim_len)
+    
+    if dim_len == 3:
+        lonx = []
+        laty = []
+        
+        idx = np.abs(dset['Longitude'][:] - lon.min()).argmin()
+        if idx > 0:
+            idx = idx - 1
+        lonx.append(idx)   
+        idx = np.abs(dset['Longitude'][:] - lon.max()).argmin()
+        if idx < len(dset['Longitude'][:]):
+            idx = idx + 1
+        lonx.append(idx)
+    #     print(lonx)
+
+        idx = np.abs(dset['Latitude'][:] - lat.min()).argmin()
+        if idx < len(dset['Latitude'[:]]):
+            idx = idx + 1
+        laty.append(idx)
+        idx = np.abs(dset['Latitude'][:] - lat.max()).argmin()
+        if idx > 0:
+            idx = idx - 1
+        laty.append(idx)
+    #     print(laty)
+    
+    ds = nc.Dataset(os.path.splitext(dsetName)[0]+'_filt.nc','w',format='NETCDF4_CLASSIC')
+    Longitude = ds.createDimension('Longitude',lonx[1]-lonx[0])
+    Latitude = ds.createDimension('Latitude',laty[0]-laty[1])
+    
+    countUp = 0
+    for key in keys:
+        try:
+            test = dset[key]
+            countUp += 1
+            if dim_len == 3:
+                value = dset[key][:,lonx[0]:lonx[1],laty[1]:laty[0]]
+                if countUp == 1:
+                    Day = ds.createDimension('Day',len(dset[key][:,1,1]))
+                var = ds.createVariable(key, 'f4', ('Day', 'Longitude', 'Latitude',))
+                var[:,:,:] = value
+            else:
+                value = dset[key][:,:,lonx[0]:lonx[1],laty[1]:laty[0]]
+                if countUp == 1:
+                    Day = ds.createDimension('Day',len(dset[key][:,1,1,1]))
+                    Stats = ds.createDimension('Stats',len(dset[key][1,:,1,1]))
+                var = ds.createVariable(key, 'f4', ('Day', 'Stats', 'Longitude', 'Latitude'))
+                var[:,:,:,:] = value
+        except:
+            print("An exception occurred")
+    
+    print(lonx)
+    print(laty)
+    dset.close()
+    ds.close()
+    
+    os.remove(dsetName)
+    
+    return lonx, laty
+    
 
 class boxMigrate:
     
@@ -24,14 +94,26 @@ class boxMigrate:
         
         return client, target
     
-    def download_data(root_directory,subdirs,client,out_path):
+    def download_data(root_directory,subdirs,client,out_path,**kwargs):
         folder = root_directory
         for subdir in subdirs:
             folder = iterate_download(folder,subdir)
         files = folder.get_items()
+        lonx = []
+        laty = []
         for file in files:
             with open(out_path+file.name,'wb') as open_file:
                 client.file(file.id).download_to(open_file)
+                
+                if kwargs.get('filter', None) == 'latlon':
+                    keys = kwargs.get('keys')
+                    lon = kwargs.get('lon')
+                    lat = kwargs.get('lat')
+                    
+                    fname = open_file.name
+                    open_file.close()
+                    lonx,laty = filterNC_latlon(fname,keys,lat,lon,laty,lonx)
+        return
     
 
     
